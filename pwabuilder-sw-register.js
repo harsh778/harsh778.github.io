e73a7a909bad7b6944215a8d58b28212a3a00dad
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-05-31 16:51:12 +0530

    Delete pwabuilder-sw-register.js

--- pwabuilder-sw-register.js
+++
@@ -1,7 +0,0 @@
-// This is the "Offline page" service worker
-
-
-import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
-
-const el = document.createElement('pwa-update');
-document.body.appendChild(el);
