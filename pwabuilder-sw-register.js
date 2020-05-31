de048cde604aa6474fa67bf5de527579997cb170
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-05-31 16:38:44 +0530

    Create pwabuilder-sw-register.js

---
+++ pwabuilder-sw-register.js
@@ -0,0 +1,7 @@
+// This is the "Offline page" service worker
+
+
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
