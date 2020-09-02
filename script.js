40524aaf9387552e2c6a2dbe65fd9b9c99917f49
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-09-02 13:09:55 +0530

    cursor now changes its color on dark mode toggle

--- script.js
+++ script.js
@@ -44,6 +44,7 @@ const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    document.getElementById("Cursor").style.filter = "invert(100)";
     document.getElementById("skill-img").style.filter = "invert(100)";
     document.getElementById("skill-img-1").style.filter = "invert(100)";
     document.getElementById("skill-img-2").style.filter = "invert(100)";
@@ -51,6 +52,7 @@ function switchTheme(e) {
 
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    document.getElementById("Cursor").style.filter = "invert(0)";
     document.getElementById("skill-img").style.filter = "invert(0)";
     document.getElementById("skill-img-1").style.filter = "invert(0)";
     document.getElementById("skill-img-2").style.filter = "invert(0)";
@@ -62,6 +64,7 @@ toggleSwitch.addEventListener('change', switchTheme, false);
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    document.getElementById("Cursor").style.filter = "invert(100)";
     document.getElementById("skill-img").style.filter = "invert(100)";
     document.getElementById("skill-img-1").style.filter = "invert(100)";
     document.getElementById("skill-img-2").style.filter = "invert(100)";
@@ -69,6 +72,7 @@ function switchTheme(e) {
     localStorage.setItem('theme', 'dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    document.getElementById("Cursor").style.filter = "invert(0)";
     document.getElementById("skill-img").style.filter = "invert(0)";
     document.getElementById("skill-img-1").style.filter = "invert(0)";
     document.getElementById("skill-img-2").style.filter = "invert(0)";
@@ -86,6 +90,7 @@ if (currentTheme) {
     toggleSwitch.checked = true;
 
     document.getElementById("skill-img").style.filter = "invert(100)";
+    document.getElementById("Cursor").style.filter = "invert(100)";
     document.getElementById("skill-img-1").style.filter = "invert(100)";
     document.getElementById("skill-img-2").style.filter = "invert(100)";
     document.getElementById("skill-img-3").style.filter = "invert(100)";
