add730ec606bbc8781aee737f0006f990fa20ab9
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-10-01 18:50:34 +0530

    tRyiNg if thiS wOrkS

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "var(--font-color);"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "var(--font-color);"
         "opacity": 0.4,
         "width": 1
       },
