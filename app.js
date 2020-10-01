01aefdb6915ae68b0685ca0084c35c4297891f61
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-10-01 18:52:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "var(--font-color);"
+        "value": "#ffffff"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "var(--font-color);"
+        "color": "#ffffff",
         "opacity": 0.4,
         "width": 1
       },
