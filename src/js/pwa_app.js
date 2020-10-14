aa6f86e56ea49fcc57110a54b3954c45983c885a
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-10-15 00:16:42 +0530

    Create pwa_app.js

---
+++ src/js/pwa_app.js
@@ -0,0 +1,6 @@
+if('serviceWorker' in navigator){
+ 
+  navigator.serviceWorker.register('sw.js')
+    .then(reg => console.log('service worker registered. GG Backend!! SW SUCCESS.'))
+    .catch(err => console.log('service worker not registered;(', err));
+}
