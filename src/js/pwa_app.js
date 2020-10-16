02e7c82a0d9b5f4bd7c60af487aa59afdc90693b
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-10-16 11:34:02 +0530

    ight i get it

--- src/js/pwa_app.js
+++ src/js/pwa_app.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
  
-  navigator.serviceWorker.register('sw.js')
+  navigator.serviceWorker.register('src/js/sw.js')
     .then(reg => console.log('service worker registered. GG Backend!! SW SUCCESS.'))
     .catch(err => console.log('service worker not registered;(', err));
 }
