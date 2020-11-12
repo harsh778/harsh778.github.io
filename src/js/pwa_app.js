03555e22aa460a822a5d06ce085842b8dc4a895d
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2020-11-12 15:43:09 +0530

    Delete pwa_app.js

--- src/js/pwa_app.js
+++
@@ -1,6 +0,0 @@
-if('serviceWorker' in navigator){
- 
-  navigator.serviceWorker.register('src/js/sw.js')
-    .then(reg => console.log('service worker registered. GG Backend!! SW SUCCESS.'))
-    .catch(err => console.log('service worker not registered;(', err));
-}
